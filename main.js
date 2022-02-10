3de894c6f6f82bdda54702c7b73e6d8ee1cc4f30
IvanATyukin <98814690+IvanATyukin@users.noreply.github.com>
2022-02-10 10:23:22 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-let a = []
-let b = []
-for (i=2; i<10;i++){
-    for (j=1; j<10;j++){
+const body = document.querySelector('body')
 
-        b.push(i,'\u00A0', '*','\u00A0', j,'\u00A0', '=', '\u00A0',Number(i*j), ' ')
+
+for (let j = 1; j < 10; j++) {
+    const ul = document.createElement('ul')
+    body.appendChild(ul)
+    for (let i = 1; i <= 10; i++) {
+        const result = j * i
+        let li = document.createElement('li')
+        li.textContent = `${j} * ${i} = ${result}`;
+        ul.appendChild(li)
     }
-a.push(b)
-b = []
 }
-document.querySelector('.n2').textContent += a[0].join('') 
-document.querySelector('.n3').textContent += a[1].join('') 
-document.querySelector('.n4').textContent += a[2].join('') 
-document.querySelector('.n5').textContent += a[3].join('') 
-document.querySelector('.n6').textContent += a[4].join('') 
-document.querySelector('.n7').textContent += a[5].join('') 
-document.querySelector('.n8').textContent += a[6].join('') 
-document.querySelector('.n9').textContent += a[7].join('') 
